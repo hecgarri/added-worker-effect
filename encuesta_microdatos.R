53b736357c7e5e231423b6007da4ae46eecd74ca
hecgarri <hectorgarridohenriquez@gmail.com>
2017-07-21 15:49:49 -0400

    Nuevos elementos. Recién creados

---
+++ encuesta_microdatos.R
@@ -0,0 +1,7 @@
+
+
+dir = "/home/hector/GoogleDriveUBB/OLR Ñuble - Observatorio laboral de Ñuble/Bases de datos/Encuesta de ocupación y desocupación en el gran Santiago/"
+
+setwd(dir)
+
+list.files(pattern = "^sept")
