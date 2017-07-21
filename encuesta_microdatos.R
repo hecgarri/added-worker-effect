c44adebfa90a5a3958e5ae05b67e6d91c0ff8369
hecgarri <hectorgarridohenriquez@gmail.com>
2017-07-21 16:31:47 -0400

    actualizo archivo

--- encuesta_microdatos.R
+++ encuesta_microdatos.R
@@ -1,7 +1,19 @@
 
+rm(list=ls())
 
 dir = "/home/hector/GoogleDriveUBB/OLR Ñuble - Observatorio laboral de Ñuble/Bases de datos/Encuesta de ocupación y desocupación en el gran Santiago/"
 
 setwd(dir)
 
-list.files(pattern = "^sept")
+a = list.files(pattern = "^mar")[18:36]
+b = list.files(pattern = "^jun")[41:58]
+c = list.files(pattern = "^sept")[1:18]
+d = list.files(pattern = "^dic")[1:17]
+
+x = vector(class(a), length(a)+length(b)+length(c)+length(d))
+
+x[c(TRUE, FALSE, FALSE, FALSE)] <- a
+x[c(FALSE, TRUE, FALSE, FALSE)] <- b
+x[c(FALSE, FALSE, TRUE, FALSE)] <- c
+x[c(FALSE, FALSE, FALSE, TRUE)] <- d
+
